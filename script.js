d83401c9501b8a40fe4ae08bdb2740d1f8f4c774
Nardeen <58826031+nardeem@users.noreply.github.com>
2021-04-29 14:41:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+////// contact button //////////
+function SendInquiry(){
+    alert("Thaks, your inquiry has sent");
+}
+////////// search //////////////
+function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+  }
+  /////////// About Read More///////////////////
+  function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+///////////////////// cards pages ////////////////////////
+/*myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "inline";
+  }
+}*/
